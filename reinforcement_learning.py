f6f3b81e9971b129ded1dfacce023b18a434a7d5
Daniel Su <sudan@live.unc.edu>
2020-04-23 11:13:45 -0400

    Successfully read in all inputs into States and Actions

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,36 +1,72 @@
 from random import random
 
+
 class MarkovDecisionProcess():
     def __init__(self, file_path):
-        self.read_inputs(file_path)
+        self.process_inputs(file_path)
 
-    def read_inputs(self, file_path):
+    def process_inputs(self, file_path):
         f = open(file_path, "r")
-        actions = self.process_action_input(MarkovDecisionProcess.read_new_line(f))
-        for action in actions:
-            print(action.cur_state + action.probability)
+        all_inputs = MarkovDecisionProcess.read_new_line(f)
+        self.actions = MarkovDecisionProcess.process_action_input(all_inputs)
+        self.state_names = MarkovDecisionProcess.get_state_names(all_inputs)
+        self.states = MarkovDecisionProcess.init_states(self.state_names, self.actions)
+        self.print_state_variables()
     
-    def process_action_input(self, inputs):
+    def print_state_variables(self):
+        for state in self.states:
+            print (state.name)
+            for action in state.possible_actions:
+                print (action.cur_state, action.action, action.new_state, action.probability)
+            print()
+
+    @staticmethod
+    def process_action_input(raw_inputs):
         actions = []
-        for input in inputs:
-            unprocessed_actions = input.split("/")
-            action = Action(unprocessed_actions[0], unprocessed_actions[1], unprocessed_actions[2], unprocessed_actions[3])
+        for input in raw_inputs:
+            unprocessed_inputs = input.split("/")
+            action = Action(
+                unprocessed_inputs[0], unprocessed_inputs[1], unprocessed_inputs[2], unprocessed_inputs[3])
             actions.append(action)
         return actions
 
-    @staticmethod    
+    @staticmethod
+    def get_state_names(raw_inputs):
+        state_names = set()
+        for input in raw_inputs:
+            unprocessed_inputs = input.split("/")
+            state_names.add(unprocessed_inputs[0])
+            state_names.add(unprocessed_inputs[2])
+        return state_names
+
+    @staticmethod
+    def init_states(state_names, all_actions):
+        states = []
+        for state_name in state_names:
+            states.append(MarkovDecisionProcess.create_state(
+                state_name, all_actions))
+        return states
+
+    @staticmethod
+    def create_state(state_name, all_actions):
+        actions = list(filter(lambda action: action.cur_state ==
+                              state_name, all_actions))
+        new_state = State(state_name, actions)
+        return new_state
+
+    @staticmethod
     def read_new_line(file):
         all_inputs = []
         while True:
-            try: 
+            try:
                 c_input = file.readline().rstrip().lstrip()
-                if c_input.strip() != '': 
+                if c_input.strip() != '':
                     all_inputs.append(c_input)
                 else:
                     break
             except ValueError:
-                print ("Invalid Input")
-        return all_inputs 
+                print("Invalid Input")
+        return all_inputs
 
 
 class Action():
@@ -40,22 +76,18 @@ class Action():
         self.probability = prob
         self.action = action
 
+
 class State():
     # Continue until all children are not -1
     # Use action score to weight probability of choosing that action
-    actions = {} # at, past, left init with -1
+    actions = {}  # at, past, left init with -1
     name = ""
-    def __init__(self, actions, name):
-        self.actions = actions
+
+    def __init__(self, name, actions):
+        self.possible_actions = actions
         self.name = name
 
 
-
-
-
 if __name__ == "__main__":
-    print("Successfully ran")
     markov_decision_process = MarkovDecisionProcess("test_data.txt")
-
-
 
